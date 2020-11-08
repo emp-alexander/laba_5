0c4a758c4424c36ebdfe055656ff0a3cdd9f130e
Alexander <borisov2261@gmail.com>
2020-11-08 18:01:34 +0700

    Add array

--- Main.java
+++ Main.java
@@ -2,16 +2,20 @@
 
 public class Main {
     public static void main (String[] args) {
-        System.out.println("First auto");
-        Auto_show first_auto = new Auto_show();
+		//Scanner in = new Scanner(System.in);
+		System.out.println("������ auto");
+        Auto_show[] first_auto = new Auto_show[10];
         Engine autoeng = new Engine(4, 2, 100);
 
-        first_auto.init("Lada", "granta", 300, 150, 2017, autoeng);
-        first_auto.display();
 
-        System.out.println("Second auto");
-        Auto_show second_auto = new Auto_show();
-        second_auto.read();
-        second_auto.display();
+
+        for(int i = 0; i < 10; i++)
+        {
+            first_auto[i] = new Auto_show();
+            first_auto[i].init("lada", "priora", 100, 100, 2018, autoeng);
+        }
+
+        first_auto[1].display();
+
     }
 }
